1625304aca7ab4231524464be739d602477db95b
wiggin77 <wiggin77@warpmail.net>
2018-01-13 15:07:14 -0500

    crypto wip

--- encrypt.go
+++ encrypt.go
@@ -1,17 +1,21 @@
 package aes
 
 import (
+	"crypto/sha512"
 	"crypto/aes"
 	"io"
 )
 
 func Encrypt(r io.Reader, w io.Writer, skey string) error {
 	plainbuf := make([]byte, aes.BlockSize)
-	key := []byte(skey)
-
+	
 	// key must be hashed to 32 bytes for AES256
+	h := sha512.New512_256()
+	key := h.Sum([]byte(skey))	
 
-	//h := sha512.New()  -- use h.Write([]byte) to add bytes to the hash. Use h.Sum(nil) to fetch the 64 byte hash.
+	// create sha512 of the plaintext file contents 
+	h = sha512.New()
+	//  -- use h.Write([]byte) to add bytes to the hash. Use h.Sum(nil) to fetch the 64 byte hash.
 
 	block, err := aes.NewCipher(key)
 	if err != nil {
